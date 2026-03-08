9180641a6d2a207f58a91893cd9ab147de8260e2
Wealthcoop <99620710+Wealthcoop@users.noreply.github.com>
2026-03-07 23:15:15 -0800

    Fix formatting in button size definitionsFix syntax error in button.tsx component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",v0.3: Add Button UI component
+        default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
